59e1b44926929d7d3622ec7682c39c20edb6b4dc
Jack Sleight <jacksleight@gmail.com>
2024-09-24 12:12:19 +0100

    Increase default S3 chunk size

--- config/large-assets.php
+++ config/large-assets.php
@@ -3,7 +3,7 @@
 return [
 
     's3' => [
-        'chunk_size' => env('LARGE_ASSETS_S3_CHUNK_SIZE', 10 * (1024 * 1024)),
+        'chunk_size' => env('LARGE_ASSETS_S3_CHUNK_SIZE', 100 * (1024 * 1024)),
     ],
 
     'tus' => [
